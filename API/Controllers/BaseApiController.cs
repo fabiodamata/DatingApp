e23106620a2d2fd9021f9f0246a043c9da49a51b
fabiodamata <fabio.damata@gmail.com>
2023-01-29 18:15:50 -0700

    End of sectionn 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // Get /api/users
+    public class BaseApiController : ControllerBase
+    {
+        
+        
+    }
+}
